d38e7a18f7e8a65eb64868beca8fb55a583075a7
ruttj1 <76194219+ruttj1@users.noreply.github.com>
2022-05-02 15:09:08 -0400

    converts cardNumber from a string to an array of ints. New bug introduced! app crashes when trying ot implement luhn algorithm.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from crypt import methods
+from lib2to3.pgen2.token import EQUAL
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -9,4 +10,29 @@ def index():
 
 @app.route("/check", methods=["POST"])
 def check():
-    return render_template("invalid.html")
+    #TODO
+        #Get card number from form as string
+        #store card number as an array of strings
+        cardNumber = list(request.form.get("cardNumber"))
+        #converts string to int
+        list_of_cardNumber = list(map(int, cardNumber))
+        #Start at the right most position (assume that is position 0)
+        #For every second position (every odd index position) (change increment)
+        temp = []
+        for number in reversed(list_of_cardNumber):
+            #multiply position i by 2
+            if (number % 2) != 0:
+                temp.append(cardNumber[number])
+            else: 
+                doubledValue = cardNumber[number] * 2
+                # add the first and second digit together
+                if doubledValue > 9:
+                    luhnValue = list(doubledValue)
+                    temp.append(luhnValue[0] + luhnValue[1])
+        #Add all the numbers together
+        luhnSum = 0
+        for numbers in temp:
+            luhnSum += temp[numbers]
+        if (luhnSum % 10) == 0:
+            return render_template("valid.html") 
+        return render_template("invalid.html")
